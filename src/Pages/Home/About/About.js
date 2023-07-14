935895a0bb1fc5ecd8b4b8a8651b76103f0057f2
mazhar <mazharulislam611@gmail.com>
2023-07-14 19:00:41 +0600

    services section

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -4,22 +4,22 @@ import parts from '../../../assets/images/about_us/parts.jpg';
 
 function About() {
     return (
-        <div className="hero min-h-screen bg-base-200 pb-12">
+        <div className="hero my-20">
             <div className="hero-content flex-col lg:flex-row">
             
             <div className="relative w-1/2">
                 <img src={person} alt='' className="h-full w-4/5 rounded-lg shadow-2xl" />
-                <img src={parts}  alt=''className="absolute right-5 top-3/4 w-3/5 max-w-sm rounded-lg shadow-2xl" />
+                <img src={parts}  alt=''className="absolute w-3/5 right-5 top-1/2 w-3/5 border-8 rounded-lg shadow-2xl" />
             </div>
             
             <div className="w-1/2">
-                <h6 className='text-orange-500 font-bold'>About Us</h6>
-                <h1 className="text-4xl font-bold">
+                <p className='text-orange-500 font-bold'>About Us</p>
+                <h1 className="my-3 text-4xl font-semibold">
                     We are qualified <br />
                      & of experience <br />
                       in this field
                 </h1>
-                <p className="py-6">
+                <p className="">
                     There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
                 <br></br>
                     The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div>
+            <div className='my-20 text-center'>
+                <p className='text-2xl font-bold text-orange-600'>Services</p>
+                <h1 className='text-5xl font-semibold'>Our Service Area</h1>
+                <p className='w-1/2 m-auto'>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            </div>
+        </div>
+       
+    )
+}
+
+export default Services;
