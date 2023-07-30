1bda04e20ddd896c491d1a97dfa8b1c2be44db41
mazhar <mazharulislam611@gmail.com>
2023-07-30 11:03:30 +0600

    update contact sec

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { LuCalendarClock } from "react-icons/lu";
-import { BiSolidPhoneCall } from "react-icons/bi";
-import { IoLocationOutline } from "react-icons/io5";
+import calender from "../../../assets/icons/001-timetable.png";
+import phone from "../../../assets/icons/Group 34.png";
+import location from "../../../assets/icons/Group 35.png";
 
 const Contact = () => {
     return (
         <div className='my-10'>
             <div className="bg-black flex justify-around text-white py-24 px-10 rounded-md">
                 <div className="flex items-center gap-3">
-                    <div className="text-3xl text-red-600"><LuCalendarClock /></div>
+                    <div className=""><img src={calender} /></div>
                     <div>
                         <p>We are open monday-friday</p>
-                        <h1 className="text-2xl font-bold">7:00 am - 9:00 pm</h1>
+                        <h1 className="text-2xl font-bold pt-2">7:00 am - 9:00 pm</h1>
                     </div>
                 </div>
                 <div className="flex items-center gap-3">
-                    <div className="text-3xl text-red-600"><BiSolidPhoneCall /></div>
+                    <div className=""><img src={phone} /></div>
                     <div>
                         <p>Have a question?</p>
-                        <h1 className="text-2xl font-bold">+2546 251 2658</h1>
+                        <h1 className="text-2xl font-bold pt-2">+2546 251 2658</h1>
                     </div>
                 </div>
                 <div className="flex items-center gap-3">
-                    <div className="text-3xl text-red-600"><IoLocationOutline /></div>
+                    <div className=""><img src={location} /></div>
                     <div>
                         <p>Need a repair? our address</p>
-                        <h1 className='text-2xl font-bold'>Liza Street, New York</h1>
+                        <h1 className='text-2xl font-bold pt-2'>Liza Street, New York</h1>
                     </div>
                 </div>
             </div>
