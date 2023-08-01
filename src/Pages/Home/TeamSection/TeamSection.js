67f8d87e19a76d38b8bad358a1fe9af69fc378ac
mazhar <mazharulislam611@gmail.com>
2023-08-01 11:03:30 +0600

    team slider

--- src/Pages/Home/TeamSection/TeamSection.js
+++ src/Pages/Home/TeamSection/TeamSection.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import TeamSlide from '../TeamSlide/TeamSlide';
 
 const TeamSection = () => {
+    
     return (
         <div className='py-10 my-10'>
             <div className='text-center'>
                 <p className='text-red-600 font-bold'>Team</p>
                 <h1 className='text-4xl font-bold my-3'>Meet Our Team</h1>
                 <p>the majority have suffered alteration in some form, by injected humour, or Randomised <br /> Words which don't look even slightly believable. </p>
-            </div> 
+            </div>
+
+            <div>
+                <TeamSlide></TeamSlide>
+            </div>
         </div>
     );
 };
