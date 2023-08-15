399c8a7ff2ce5474d89b8fc10c128e3ae96ccf4a
mazhar <mazharulislam611@gmail.com>
2023-08-15 19:51:09 +0600

    up

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -3,17 +3,48 @@ import checkOutBg from '../../assets/images/checkout/checkout.png';
 
 const CheckOut = () => {
     return (
+        <>
         <div className="hero mb-5 rounded-md" style={{backgroundImage: `url(${checkOutBg})`}}>
-            <div className="hero-overlay bg-opacity-60 rounded-md"></div>
+            <div className="hero-overlay bg-opacity-70 rounded-md"></div>
             <div className="hero-content text-neutral-content">
-                <div className="px-24">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                <div className="my-20">
+                    <h1 className="mb-5 text-5xl font-bold">Check Out</h1>
                 </div>
             </div>
-            
         </div>
+
+        <div className='p-8 my-5 bg-gray-200'>
+            <form>
+                <form className="card-body">
+                    <div className='grid grid-cols-2 gap-3'>
+                        <div className="form-control">
+                            <input type="text" placeholder="First Name" 
+                            className="input input-bordered input-success w-full" />
+                        </div>
+                        <div className="form-control">
+                            <input type="text" placeholder="Last Name" 
+                            className="input input-bordered input-success w-full" />
+                        </div>
+                        <div className="form-control">
+                            <input type="text" placeholder="Your Phone" 
+                            className="input input-bordered input-success w-full" />
+                        </div>
+                        <div className="form-control">
+                            <input type="text" placeholder="Your Email"  
+                            className="input input-bordered input-success w-full" />
+                        </div>
+                    </div>                    
+                    <div className="form-control">
+                        <input type="textarea" cols="50" placeholder="Your Message" className="input input-bordered" />
+                    </div>
+                    <div className="form-control mt-6">
+                        <input type='submit' className="btn btn-secondary" value="Order Confirm" />
+                    </div>
+                </form>
+            </form>
+           
+        </div>
+        </>
     );
 };
 
