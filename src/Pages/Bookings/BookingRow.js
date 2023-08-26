ad66890ce191d5c4e466e5c35a41f44abe214ed4
mazhar <mazharulislam611@gmail.com>
2023-08-26 12:40:45 +0600

    delet btn

--- src/Pages/Bookings/BookingRow.js
+++ src/Pages/Bookings/BookingRow.js
@@ -1,11 +1,46 @@
-const BookingRow = ({booking}) => {
-    const {customerName, email, date, service, price, img} = booking;
+import Swal from "sweetalert2";
+
+const BookingRow = ({ booking }) => {
+    const Swal = require('sweetalert2');
+    const { _id, customerName, email, date, service, price, img } = booking;
+
+    const handleDelete = id => {
+        const proceed = 
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+                }
+            });
+
+        if(proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'DELETE',
+
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+        }
+    }
     return (
         <tr>
             <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
+                <button onClick={() => handleDelete(_id)} className="btn btn-sm btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
             </th>
             <td>
                 <div className="avatar">
