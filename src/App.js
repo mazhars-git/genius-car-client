21cd9b7fb301aae1942ef810645167bf186c939d
mazhar <mazharulislam611@gmail.com>
2023-09-10 22:30:47 +0600

    update router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
-import './App.css';
-import router from './Router/Routes/Routes';
+import { router } from './Router/Routes/Routes';
 
 function App() {
 
