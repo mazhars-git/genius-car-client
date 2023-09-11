63b4363bb021b9588b83f1346cba0ab8c11ce805
mazhar <mazharulislam611@gmail.com>
2023-09-11 23:31:33 +0600

    update

--- src/Pages/Home/ServiceDetails/ServiceDetails.js
+++ src/Pages/Home/ServiceDetails/ServiceDetails.js
@@ -4,7 +4,7 @@ import headerImg from '../../../assets/images/checkout/checkout.png';
 const ServiceDetails = () => {
     return (
         <div>
-            <div className='py-16 my-5' style={{backgroundImage: `url(${headerImg})`}}>
+            <div className='py-32 my-5 rounded-md' style={{backgroundImage: `url(${headerImg})`}}>
                 <h1 className='text-3xl ml-20 font-semibold align-center text-white'>Service Details</h1>
             </div>
             
