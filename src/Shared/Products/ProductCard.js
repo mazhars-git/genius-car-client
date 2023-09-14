28f7f41a176abf50302943634fb83e81475affa4
mazhar <mazharulislam611@gmail.com>
2023-09-14 23:16:02 +0600

    pd design update

--- src/Shared/Products/ProductCard.js
+++ src/Shared/Products/ProductCard.js
@@ -4,11 +4,11 @@ import { AiFillStar } from "react-icons/ai";
 const ProductCard = ({product}) => {
     const {image, price, title} = product;
     return (
-        <div className="card w-85 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
+        <div className="card w-85 shadow-md">
+            <figure className="p-8 m-5 bg-slate-100">
                 <img src={image} alt="Shoes" className="rounded-xl" />
             </figure>
-            <div className="card-body items-center text-center">
+            <div className="card-body items-center text-center ">
                 <div className='flex text-orange-500'>
                     <AiFillStar /> 
                     <AiFillStar />
