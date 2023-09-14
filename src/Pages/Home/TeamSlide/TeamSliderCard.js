f1059d08b609f1faa2ab527b0b5ece38728b1149
mazhar <mazharulislam611@gmail.com>
2023-09-14 23:00:50 +0600

    update

--- src/Pages/Home/TeamSlide/TeamSliderCard.js
+++ src/Pages/Home/TeamSlide/TeamSliderCard.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { BsFacebook, BsInstagram } from 'react-icons/bs'
-import { AiFillTwitterCircle } from 'react-icons/ai'
-import { TiSocialLinkedinCircular } from 'react-icons/ti'
 import fbIcon from '../../../assets/icons/social/fb.png';
 import twitIcon from '../../../assets/icons/social/twitter.png';
 import linkIcon from '../../../assets/icons/social/linkedin.png';
@@ -18,7 +15,7 @@ const TeamSliderCard = ({member}) => {
             <h2 className="card-title font-bold text-2xl pt-3">{title}</h2>
             <h6 className='pb-3'>{designation}</h6>
         </div>
-        <div className='flex pb-4 justify-center gap-2'>
+        <div className='flex py-4 justify-center gap-2'>
             <a href="#"><img src={fbIcon} alt="" /></a>
             <a href="#"><img src={twitIcon} alt="" /></a>
             <a href="#"><img src={linkIcon} alt="" /></a>
